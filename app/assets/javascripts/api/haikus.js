ce9b56597aab8171ca73c29d2b1a522c00f5ebea
Matt Garrison <mattsgarrison@iconoclastlabs.com>
2012-10-13 14:32:29 -0500

    added angular code

---
+++ app/assets/javascripts/api/haikus.js
@@ -0,0 +1,24 @@
+var nounApp = angular.module('NounApp', ['ngResource']);
+
+nounApp.directive('whenScrolled', [function($scope){
+  return function($scope, elm, attr){
+    var raw = elm[0];
+    elm.bind('scroll', function(){
+      if(raw.scrollTop + raw.offsetHeight >= raw.scrollHeight){
+        $scope.$apply(attr.whenScrolled);
+      }
+    });
+  };
+}]);
+
+//nounApp.factory('Nouns');
+
+nounApp.controller('NounCtrl', ['$scope', function($scope){
+    $scope.title = "Icon Library";
+    $scope.nouns = [{name:'Tacos',attribution:"Mexicans"},
+                    {name:'Burger',attribution: "USA"},
+                    {name:'Spongebob', attribution: "Crack"},
+                    {name:'Sherlock',attribution:"England"},
+                    {name:'Robot',attribution: "Chicken"}];
+}]);
+
