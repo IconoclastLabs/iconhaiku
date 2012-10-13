3363e8cdb37d6bfef02301c3395ad6b32c08f227
Matt Garrison <mattsgarrison@iconoclastlabs.com>
2012-10-12 20:20:22 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IconHaiku::Application.config.secret_token = 'd6af25c97a3aa62ec92d0acddcd9df5228bcccbd586f41182dcd31c3cf3de155ce25b2d374569582ff6906f1401d961c46f40e8574d65928e47bc68e3f38f5c3'
